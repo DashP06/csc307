413b36b021371a4f31c8c7b13f19832820eed49f
DashP06 <dashzoop@gmail.com>
2025-10-07 18:27:02 -0700

    step 6

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -44,6 +44,17 @@ const findUserById = (id) =>
 
 app.use(express.json());
 
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
